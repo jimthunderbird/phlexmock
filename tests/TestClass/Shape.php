67bb77bcd40d97a72433af281a6e9a55385306b9
jimthunderbird <jim.javathunderbird@gmail.com>
2015-03-23 21:47:07 -0700

    add unit tests

---
+++ tests/TestClass/Shape.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace TestClass;
+
+class Shape 
+{
+    private $x;
+
+    private static $value;
+
+    public function info()
+    {
+        return "This is a Shape Class";
+    }
+}
