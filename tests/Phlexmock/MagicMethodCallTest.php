092537d098c9dba8550e38376b1e9e75cf8be763
jimthunderbird <jim.javathunderbird@gmail.com>
2015-03-25 18:26:05 -0700

    separate magic method call test between dynamic and static

--- tests/Phlexmock/MagicMethodCallTest.php
+++ tests/Phlexmock/MagicMethodCallTest.php
@@ -22,7 +22,13 @@ class MagicMethodCallTest extends TestCase
         $obj = new \TestClass\MethodMissingCatcher();
         $result = $obj->randomMethod();
         $this->assertEquals($result, "caught missing dynmaic method randomMethod");
+    }
 
+    /**
+     * test the magic method call __callStatic 
+     */
+    public function testStaticMagicMethodCall()
+    {
         $result = \TestClass\MethodMissingCatcher::randomStaticMethod();
         $this->assertEquals($result, "caught missing static method randomStaticMethod");
     }
